e081210e55ae66878e9fc39517ec379675640e5d
Ariel Conti <arielconti10@gmail.com>
2021-09-08 20:01:34 -0300

    feat: added theme and styled components type

---
+++ src/global/theme.ts
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    primary: '#1B31A8',
+    secondary: '#0079FF',
+
+    blueGray50: '#E9EEF2',
+    blueGray600: '#4D6475',
+    blueGray900: '#1E2A32',
+
+    shape: '#FFFFFF',
+    background: '#F4F8FA',
+  },
+};
+
+export default theme;
