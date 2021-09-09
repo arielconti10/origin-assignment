e47d0122768d2e598bc87852774d3f53ecf8d640
Ariel Conti <arielconti10@gmail.com>
2021-09-09 10:20:24 -0300

    feat: created components on Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
 
 import logo from '../../assets/images/logo.svg';
+import house from '../../assets/icons/buy-a-house.svg';
 
-import { Container, Header, LogoImage, Title, BoldText, Card } from './styles';
+import {
+  Container,
+  Header,
+  LogoImage,
+  Title,
+  BoldText,
+  Card,
+  Icon,
+  TitleTextContainer,
+  CardTitleContainer,
+  CardTitle,
+  CardSubTitle,
+  Form,
+  Fields,
+  InputContainer,
+  InputLabel,
+  Input,
+  AmountCard,
+  AmountContainer,
+  AmountText,
+  AmountValue,
+  AmountDetail,
+  AmountDetailText,
+  Button,
+} from './styles';
 
 export function Home(): JSX.Element {
   return (
@@ -14,7 +39,45 @@ export function Home(): JSX.Element {
       <Title>
         Let&apos;s plan your <BoldText>saving goal.</BoldText>
       </Title>
-      <Card></Card>
+
+      <Card>
+        <CardTitleContainer>
+          <Icon src={house} />
+          <TitleTextContainer>
+            <CardTitle>Buy a house</CardTitle>
+            <CardSubTitle>Saving goal</CardSubTitle>
+          </TitleTextContainer>
+        </CardTitleContainer>
+
+        <Form>
+          <Fields>
+            <InputContainer>
+              <InputLabel>Total Amount</InputLabel>
+              <Input placeholder="24,000" />
+            </InputContainer>
+
+            <InputContainer>
+              <InputLabel>Reach Goal By</InputLabel>
+              <Input placeholder="October 2021" />
+            </InputContainer>
+          </Fields>
+
+          <AmountCard>
+            <AmountContainer>
+              <AmountText>MonthlyAmount</AmountText>
+              <AmountValue>$521</AmountValue>
+            </AmountContainer>
+          </AmountCard>
+          <AmountDetail>
+            <AmountDetailText>
+              You&apos;re planing 48 monthly deposits to reach your $25,000 goal
+              by October 2020.
+            </AmountDetailText>
+          </AmountDetail>
+
+          <Button>Confirm</Button>
+        </Form>
+      </Card>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import styled from 'styled-components';
+import styled, { ThemeContext } from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
@@ -35,4 +35,144 @@ export const BoldText = styled.span`
   font-weight: bold;
 `;
 
-export const Card = styled.div``;
+export const Card = styled.div`
+  width: 560px;
+  background-color: ${({ theme }) => theme.colors.shape};
+
+  box-shadow: 0px 16px 32px rgba(30, 42, 50, 0.08);
+  border-radius: 8px;
+
+  padding: 35px 40px;
+  margin-top: 24px;
+`;
+
+export const CardTitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+export const CardTitle = styled.p`
+  font-family: ${({ theme }) => theme.fonts.alternative};
+  font-size: 24px;
+
+  color: ${({ theme }) => theme.colors.blueGray900};
+
+  margin-bottom: 8px;
+`;
+export const CardSubTitle = styled.p`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: 16px;
+
+  color: ${({ theme }) => theme.colors.blueGray400};
+`;
+
+export const TitleTextContainer = styled.div``;
+
+export const Icon = styled.img`
+  margin-right: 16px;
+`;
+
+export const Form = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 24px;
+`;
+
+export const Fields = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const InputContainer = styled.div``;
+
+export const InputLabel = styled.label`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.blueGray900};
+`;
+
+export const Input = styled.input`
+  font-family: ${({ theme }) => theme.fonts.alternative};
+  font-size: 24px;
+
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.colors.blueGray50};
+
+  width: 100%;
+  padding: 14px 13px;
+  margin-top: 5px;
+
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.blueGray600};
+  }
+`;
+
+export const AmountCard = styled.div`
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.colors.blueGray50};
+  border-bottom: 0;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+
+  padding: 24px 32px;
+  margin-top: 24px;
+`;
+
+export const AmountContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AmountText = styled.p`
+  flex: 1;
+
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: 20px;
+`;
+
+export const AmountValue = styled.div`
+  font-family: ${({ theme }) => theme.fonts.alternative};
+  font-size: 32px;
+
+  color: ${({ theme }) => theme.colors.secondary};
+`;
+
+export const AmountDetail = styled.div`
+  background-color: ${({ theme }) => theme.colors.background};
+  flex: 1;
+
+  border: 1px solid ${({ theme }) => theme.colors.blueGray50};
+  border-radius: 4px;
+  border-top: 0;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+`;
+
+export const AmountDetailText = styled.p`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: 12px;
+  padding: 24px 32px;
+`;
+
+export const Button = styled.div`
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  width: 320px;
+
+  border-radius: 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.shape};
+
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: 16px;
+
+  padding: 18px 0;
+  margin-top: 50px;
+`;
